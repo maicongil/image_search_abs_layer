65e7db581df32090e9a4008c2c47b8f4eea8632c
Maicon <maicon-gil@PRO-PAE-0483.PROCERGS.REDERS>
2017-02-07 13:56:17 -0200

    Routes definition

--- server.js
+++ server.js
@@ -17,6 +17,21 @@ app.get("/", function(req, res){
     res.sendFile(__dirname+"/index.html");
 });
 
+app.get("/search", function(req, res){
+    //User Story: I can get a list of the most recently submitted search strings.
+    res.send("THIS IS LIST OF SEARCH");
+});
+
+app.get("/search/:searchString", function(req, res){
+    //User Story: I can get the image URLs, alt text and page urls for a set of images relating to a given search string
+    //User Story: I can paginate through the responses by adding a ?offset=2 parameter to the URL.
+    res.send("SEARCHSTRING: "+ req.params.searchString + " OFFSET: "+ req.query.offset);
+});
+
+app.get("*", function(req, res){
+    res.send("Not found");
+});
+
 
 app.listen(port, function(){
     console.log("Server is listening on port "+port);
