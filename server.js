c87fcbe13e95a9c8ce5437076eaa0e95d7a62108
Maicon <maicon-gil@PRO-PAE-0483.PROCERGS.REDERS>
2017-02-14 08:40:47 -0200

    Implemented search img on bing

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require("express"),
     path = require("path"),
     app = express(),
     port = process.env.PORT || 8000,
+    bingSearch = require("./lib/bing-search.js"),
+    dataParser = require("./lib/data-parser.js"),
     mongoUrl = process.env.MONGO_URL || "mongodb://localhost/image_search_abs_layer";
 
 mongoose.connect(mongoUrl);
@@ -25,7 +27,14 @@ app.get("/search", function(req, res){
 app.get("/search/:searchString", function(req, res){
     //User Story: I can get the image URLs, alt text and page urls for a set of images relating to a given search string
     //User Story: I can paginate through the responses by adding a ?offset=2 parameter to the URL.
-    res.send("SEARCHSTRING: "+ req.params.searchString + " OFFSET: "+ req.query.offset);
+    var returnData ={
+        searchString : req.params.searchString,
+        offset : req.query.offset,
+        data : []};
+    bingSearch.searchImages(req.params.searchString, req.query.offset).then(function(data){
+        returnData.data = dataParser.parseBingApi(data);
+        res.send(JSON.stringify(returnData));
+    });
 });
 
 app.get("*", function(req, res){
--- test/test-bing-search.js
+++ test/test-bing-search.js
@@ -1,6 +1,7 @@
 var chai = require('chai');
 var expect = chai.expect;
 var BingSearch = require("../lib/bing-search.js");
+var data = require("../mock_data.json");
 
 chai.use(require('chai-things'));
 
@@ -12,7 +13,20 @@ describe('BingSearch', function(){
       expect(function(){BingSearch.searchImages()}).to.throw('The search parameter was not informed.');
     });
 
-    
+    it('should return fetched data', function(){
+     
+      fetcherStub = function() {
+            return Promise.resolve(data);
+      };
+
+      var expected = data;
+
+      return BingSearch.searchImages("img", 2, fetcherStub).then(function(result){
+        expect(result).to.be.eql(expected);
+      });
+      
+    });
+
   });
 });
 
