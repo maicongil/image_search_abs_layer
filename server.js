1d885922c34d5b976b71fed8068fc4880175ec19
Maicon <maicon-gil@PRO-PAE-0483.PROCERGS.REDERS>
2017-02-14 09:42:05 -0200

    Implement search persistence and list

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var express = require("express"),
     port = process.env.PORT || 8000,
     bingSearch = require("./lib/bing-search.js"),
     dataParser = require("./lib/data-parser.js"),
+    searchModel = require("./models/search.js"),
     mongoUrl = process.env.MONGO_URL || "mongodb://localhost/image_search_abs_layer";
 
 mongoose.connect(mongoUrl);
@@ -13,27 +14,52 @@ mongoose.connect(mongoUrl);
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
-
 //ROUTES
 app.get("/", function(req, res){
-    res.sendFile(__dirname+"/index.html");
+    res.sendFile(__dirname+"/public/index.html");
 });
 
 app.get("/search", function(req, res){
     //User Story: I can get a list of the most recently submitted search strings.
-    res.send("THIS IS LIST OF SEARCH");
+
+    searchModel.find({}).
+    limit(10).
+    sort({ searchDate: -1 }).
+    exec(function(error, results){
+        if(error){
+            console.log(error);
+            res.send(JSON.stringify({error : error}));
+        }
+        
+        var formatedResults = results.map(function(item){
+            return {searchString : item.searchString, searchDate : item.searchDate};
+        });
+
+        res.send(JSON.stringify(formatedResults));
+    });
 });
 
 app.get("/search/:searchString", function(req, res){
     //User Story: I can get the image URLs, alt text and page urls for a set of images relating to a given search string
     //User Story: I can paginate through the responses by adding a ?offset=2 parameter to the URL.
-    var returnData ={
-        searchString : req.params.searchString,
-        offset : req.query.offset,
-        data : []};
-    bingSearch.searchImages(req.params.searchString, req.query.offset).then(function(data){
-        returnData.data = dataParser.parseBingApi(data);
-        res.send(JSON.stringify(returnData));
+
+    searchModel.create({searchString : req.params.searchString}, function(error, createdSearch){
+        if(error){
+            console.log(error);
+            res.send(JSON.stringify({error : error}));
+        }
+
+        var returnData ={
+            searchString : req.params.searchString,
+            offset : req.query.offset,
+            data : []
+        };
+
+        bingSearch.searchImages(req.params.searchString, req.query.offset).then(function(data){
+            returnData.data = dataParser.parseBingApi(data);
+            res.send(JSON.stringify(returnData));
+        });
+
     });
 });
 
